1abaf1b11dfc8fc54723252c32ef0c24432ebac5
Nick Brown <ndb@live.com.au>
2024-07-29 17:44:12 +1000

    Moved zone-ts deps across to nextjs-supabase starter

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,
+    modularizeImports: {
+        '@mui/icons-material': {
+            transform: '@mui/icons-material/{{member}}',
+        },
+        '@mui/material': {
+            transform: '@mui/material/{{member}}',
+        },
+        '@mui/lab': {
+            transform: '@mui/lab/{{member}}',
+        },
+    },
+    experimental: {
+        serverComponentsExternalPackages: ["@node-rs/argon2"]
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+};
 
 module.exports = nextConfig;
